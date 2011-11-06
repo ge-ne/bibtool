4cc0f721282572a2938581fe485c186d6964577c
gene <gene>
2011-11-06 19:48:02 +0000

    testcase added

--- Test/rewrite.pl
+++ Test/rewrite.pl
@@ -1,6 +1,6 @@
 #!/bin/perl -W
 #******************************************************************************
-# $Id: rewrite.pl,v 1.4 2011-10-22 06:08:31 gene Exp $
+# $Id: rewrite.pl,v 1.5 2011-11-06 19:48:02 gene Exp $
 # =============================================================================
 #  
 #  This file is part of BibTool.
@@ -39,7 +39,7 @@ use BUnit;
 BUnit::run(name => 'rewrite_add_field_1',
     args	      => 'x1.bib',
     resource 	      => 'add.field={xyzzy="abc"}',
-    expected_out      => <<EOF,
+    expected_out      => <<__EOF__,
 
 \@Manual{	  bibtool,
   title		= {BibTool},
@@ -47,14 +47,14 @@ BUnit::run(name => 'rewrite_add_field_1',
   year		= 2011,
   xyzzy		= {abc}
 }
-EOF
+__EOF__
     expected_err => '' );
 
 #------------------------------------------------------------------------------
 BUnit::run(name => 'rewrite_add_field_2',
     args	      => 'x1.bib',
     resource 	      => 'add.field={xyzzy={abc}}',
-    expected_out      => <<EOF,
+    expected_out      => <<__EOF__,
 
 \@Manual{	  bibtool,
   title		= {BibTool},
@@ -62,14 +62,14 @@ BUnit::run(name => 'rewrite_add_field_2',
   year		= 2011,
   xyzzy		= {abc}
 }
-EOF
+__EOF__
     expected_err => '' );
 
 #------------------------------------------------------------------------------
 BUnit::run(name => 'rewrite_add_field_3',
     args	      => 'x1.bib',
     resource 	      => 'add.field={xyzzy=abc}',
-    expected_out      => <<EOF,
+    expected_out      => <<__EOF__,
 
 \@Manual{	  bibtool,
   title		= {BibTool},
@@ -77,95 +77,95 @@ BUnit::run(name => 'rewrite_add_field_3',
   year		= 2011,
   xyzzy		= {abc}
 }
-EOF
+__EOF__
     expected_err => '' );
 
 #------------------------------------------------------------------------------
 BUnit::run(name => 'rewrite_delete_field_1',
     args	 => 'x1.bib',
     resource 	 => 'delete.field={xyzzy}',
-    expected_out => <<EOF,
+    expected_out => <<__EOF__,
 
 \@Manual{	  bibtool,
   title		= {BibTool},
   author	= {Gerd Neugebauer},
   year		= 2011
 }
-EOF
+__EOF__
     expected_err => '' );
 
 #------------------------------------------------------------------------------
 BUnit::run(name => 'rewrite_delete_field_2',
     args	 => 'x1.bib',
     resource 	 => 'delete.field={year}',
-    expected_out => <<EOF,
+    expected_out => <<__EOF__,
 
 \@Manual{	  bibtool,
   title		= {BibTool},
   author	= {Gerd Neugebauer}
 }
-EOF
+__EOF__
     expected_err => '' );
 
 #------------------------------------------------------------------------------
 BUnit::run(name => 'rewrite_rewrite_1',
     args	 => 'x1.bib',
     resource 	 => 'rewrite.rule={"01"}',
-    expected_out => <<EOF,
+    expected_out => <<__EOF__,
 
 \@Manual{	  bibtool,
   title		= {BibTool},
   author	= {Gerd Neugebauer}
 }
-EOF
+__EOF__
     expected_err => '' );
 
 #------------------------------------------------------------------------------
 BUnit::run(name => 'rewrite_rewrite_2',
     args	 => 'x1.bib',
     resource 	 => 'rewrite.rule={"G.* N[a-z]*" # "A.U. Thor"}',
-    expected_out => <<EOF,
+    expected_out => <<__EOF__,
 
 \@Manual{	  bibtool,
   title		= {BibTool},
   author	= {A.U. Thor},
   year		= 2011
 }
-EOF
+__EOF__
     expected_err => '' );
 
 #------------------------------------------------------------------------------
 BUnit::run(name => 'rewrite_rewrite_3',
     args	 => 'x1.bib',
     resource 	 => 'rewrite.rule={author title # "G.* N[a-z]*" # "A.U. Thor"}',
-    expected_out => <<EOF,
+    expected_out => <<__EOF__,
 
 \@Manual{	  bibtool,
   title		= {BibTool},
   author	= {A.U. Thor},
   year		= 2011
 }
-EOF
+__EOF__
     expected_err => '' );
 
 #------------------------------------------------------------------------------
 BUnit::run(name => 'rewrite_rewrite_4',
     args	 => 'x1.bib',
     resource 	 => 'rewrite.rule={title # "G.* N[a-z]*" # "A.U. Thor"}',
-    expected_out => <<EOF,
+    expected_out => <<__EOF__,
 
 \@Manual{	  bibtool,
   title		= {BibTool},
   author	= {Gerd Neugebauer},
   year		= 2011
 }
-EOF
+__EOF__
     expected_err => '' );
 
 #------------------------------------------------------------------------------
 BUnit::run(name  => 'select_by_string_1',
     args	 => '--select.by.string=\'{"none"}\' xampl.bib',
-    expected_out => <<EOF);
+    expected_out => <<__EOF__);
 \@PREAMBLE{ "\\newcommand{\\noopsort}[1]{} " 
 	 # "\\newcommand{\\printfirst}[2]{#1} " 
 	 # "\\newcommand{\\singleletter}[1]{#1} " 
@@ -173,12 +173,12 @@ BUnit::run(name  => 'select_by_string_1',
 \@STRING{acm	= "The OX Association for Computing Machinery" }
 \@STRING{stoc	= " Symposium on the Theory of Computing" }
 \@STRING{stoc-key= "OX{\\singleletter{stoc}}" }
-EOF
+__EOF__
 
 #------------------------------------------------------------------------------
 BUnit::run(name  => 'select_by_string_2',
     args	 => '--select.by.string=\'{"anual-mi"}\' xampl.bib',
-    expected_out => <<EOF);
+    expected_out => <<__EOF__);
 \@PREAMBLE{ "\\newcommand{\\noopsort}[1]{} " 
 	 # "\\newcommand{\\printfirst}[2]{#1} " 
 	 # "\\newcommand{\\singleletter}[1]{#1} " 
@@ -191,12 +191,12 @@ BUnit::run(name  => 'select_by_string_2',
   key		= "Manmaker",
   title		= "The Definitive Computer Manual"
 }
-EOF
+__EOF__
 
 #------------------------------------------------------------------------------
 BUnit::run(name  => 'select_by_string_3',
     args	 => '--select.by.string=\'{"manmaker"}\' xampl.bib',
-    expected_out => <<EOF);
+    expected_out => <<__EOF__);
 \@PREAMBLE{ "\\newcommand{\\noopsort}[1]{} " 
 	 # "\\newcommand{\\printfirst}[2]{#1} " 
 	 # "\\newcommand{\\singleletter}[1]{#1} " 
@@ -220,12 +220,12 @@ BUnit::run(name  => 'select_by_string_3',
   year		= 1986,
   note		= "This is a full MANUAL entry"
 }
-EOF
+__EOF__
 
 #------------------------------------------------------------------------------
 BUnit::run(name  => 'select_by_string_4',
     args	 => '--select.by.string=\'{"MISC"}\' xampl.bib',
-    expected_out => <<EOF);
+    expected_out => <<__EOF__);
 \@PREAMBLE{ "\\newcommand{\\noopsort}[1]{} " 
 	 # "\\newcommand{\\printfirst}[2]{#1} " 
 	 # "\\newcommand{\\singleletter}[1]{#1} " 
@@ -247,12 +247,12 @@ BUnit::run(name  => 'select_by_string_4',
   year		= 1984,
   note		= "This is a full MISC entry"
 }
-EOF
+__EOF__
 
 #------------------------------------------------------------------------------
 BUnit::run(name  => 'select_by_string_10',
     args	 => '--select.by.string=\'{"MISC"}\' xampl.bib',
-    expected_out => <<EOF);
+    expected_out => <<__EOF__);
 \@PREAMBLE{ "\\newcommand{\\noopsort}[1]{} " 
 	 # "\\newcommand{\\printfirst}[2]{#1} " 
 	 # "\\newcommand{\\singleletter}[1]{#1} " 
@@ -274,12 +274,12 @@ BUnit::run(name  => 'select_by_string_10',
   year		= 1984,
   note		= "This is a full MISC entry"
 }
-EOF
+__EOF__
 
 #------------------------------------------------------------------------------
 BUnit::run(name  => 'select_by_string_11',
     args	 => '--select.by.string=\'{note "Kn"}\' xampl.bib',
-    expected_out => <<EOF);
+    expected_out => <<__EOF__);
 \@PREAMBLE{ "\\newcommand{\\noopsort}[1]{} " 
 	 # "\\newcommand{\\printfirst}[2]{#1} " 
 	 # "\\newcommand{\\singleletter}[1]{#1} " 
@@ -292,7 +292,56 @@ BUnit::run(name  => 'select_by_string_11',
   key		= {Volume-2},
   note		= "Volume~2 is listed under Knuth \\cite{book-full}"
 }
-EOF
+__EOF__
+
+#------------------------------------------------------------------------------
+BUnit::run(name  => 'select_by_string_12',
+    args	 => '--select.by.string=\'{$type "misc"}\' xampl.bib',
+    expected_out => <<__EOF__);
+\@PREAMBLE{ "\\newcommand{\\noopsort}[1]{} " 
+	 # "\\newcommand{\\printfirst}[2]{#1} " 
+	 # "\\newcommand{\\singleletter}[1]{#1} " 
+	 # "\\newcommand{\\switchargs}[2]{#2#1} " }
+\@STRING{acm	= "The OX Association for Computing Machinery" }
+\@STRING{stoc	= " Symposium on the Theory of Computing" }
+\@STRING{stoc-key= "OX{\\singleletter{stoc}}" }
+
+\@Misc{		  misc-minimal,
+  key		= "Missilany",
+  note		= "This is a minimal MISC entry"
+}
+
+\@Misc{		  misc-full,
+  author	= "Joe-Bob Missilany",
+  title		= "Handing out random pamphlets in airports",
+  howpublished	= "Handed out at O'Hare",
+  month		= oct,
+  year		= 1984,
+  note		= "This is a full MISC entry"
+}
+
+\@Misc{		  random-note-crossref,
+  key		= {Volume-2},
+  note		= "Volume~2 is listed under Knuth \\cite{book-full}"
+}
+__EOF__
+
+#------------------------------------------------------------------------------
+#BUnit::run(name  => 'select_by_non_string_1',
+#    args         => '--select.by.non.string=\'{"aa"}\'',
+#expected_err=>'',
+#    bib	         => <<__EOF__,
+#\@article{ a,
+#  author = "aa",
+#  title	 = "the title"
+#}
+#\@article{ b,
+#  author = "ab",
+#  title	 = "the title"
+#}
+#__EOF__
+#    expected_out => <<__EOF__);
+#__EOF__
 
 #------------------------------------------------------------------------------
 # Local Variables: 
