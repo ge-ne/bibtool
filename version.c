c00816c169f18247193899e54a853dee4de8f89b
Gerd Neugebauer <gene@gerd-neugebauer.de>
2014-04-14 22:10:07 +0200

    version incremented

--- version.c
+++ version.c
@@ -22,7 +22,7 @@
 **		numbers.  In addition a postfix like |alpha| or a
 **		patchlevel like |p1| can be present.
 **___________________________________________________			     */
- char * bibtool_version = "2.56";		   /*                        */
+ char * bibtool_version = "2.57-alpha";		   /*                        */
 
 /*-----------------------------------------------------------------------------
 ** Function:	show_version()
