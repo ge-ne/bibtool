ced6eff8e2daf32f72d11b36601d09f8d033dba8
Gerd Neugebauer <gene@gerd-neugebauer.de>
2015-06-09 02:02:15 +0200

    next release

--- version.c
+++ version.c
@@ -22,7 +22,7 @@
 **		numbers.  In addition a post-fix like |alpha| or a
 **		patch level like |p1| can be present.
 **___________________________________________________			     */
- char * bibtool_version = "2.60alpha";		   /*                        */
+ char * bibtool_version = "2.60";		   /*                        */
 
 /*-----------------------------------------------------------------------------
 ** Function:	show_version()
